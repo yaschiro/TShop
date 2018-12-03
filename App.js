1d8ecae36c2a1287464d0072ebe5c6b04bf6f173
sunyankai <yaschiroza@sunyankai-PC>
2018-12-03 16:18:33 +0800

    adsfasfasdf

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import {
     createMaterialTopTabNavigator
 } from 'react-navigation';
 //页面切换动画插入器
-import CardStackStyleInterpolator from 'react-navigation/src/views/StackView/StackViewStyleInterpolator';
+//import CardStackStyleInterpolator from 'react-navigation/src/views/StackView/StackViewStyleInterpolator';
 // 矢量图
 import Icon from 'react-native-vector-icons/Ionicons';
 import theme from './src/config/theme';
@@ -133,7 +133,8 @@ const MyTab = createBottomTabNavigator({
 );
 const App = createStackNavigator({
     SplashPage: {
-        screen: SplashPage,
+        screen: SplashPage
+        ,
         navigationOptions: {
             gesturesEnabled: true,
             header: null  //去掉 react-navigation 提供的标题
@@ -155,10 +156,11 @@ const App = createStackNavigator({
     }
 }, {
     mode: 'card',// 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
-    headerMode: 'none',//// 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
-    transitionConfig: () => ({ //切换动画
+    headerMode: 'none'
+    //// 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
+   /* ,transitionConfig: () => ({ //切换动画
         screenInterpolator: CardStackStyleInterpolator.forHorizontal //水平动画
-    })
+    })*/
 });
 
 
